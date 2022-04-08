27a8bb6b715e71f98ad0eab7ce370b50c7c3b262
Safia Jotey <safiaahmed1786@gmail.com>
2022-04-08 17:30:57 +0600

    [add] all services

--- index.js
+++ index.js
@@ -18,81 +18,146 @@ const client = new MongoClient(uri, {
 
 client.connect((err) => {
   const servicesCollection = client.db('DreamTravel').collection('services');
-  const bookingsCollection = client.db('DreamTravel').collection('bookings');
-
+  // const bookingsCollection = client.db('oClock').collection('bookings');
+  // const reviewCollection = client.db('oClock').collection('Reviews');
+  // const usersCollection = client.db('oClock').collection('users');
+  // const bestProductCollection = client.db('oClock').collection('bestProduct');
+  //user
+  // app.post('/users', async (req, res) => {
+  //   const user = req.body;
+  //   const result = await usersCollection.insertOne(user);
+  //   console.log(result);
+  //   res.json(result);
+  //   res.send(result);
+  //   console.log(result);
+  // });
+  // app.put("/users", async (req, res) => {
+  //   const email=req.body;
+  //   const filter={
+  //     email:user.email
+  //   };
+  //   options={upsert:true};
+  //   const updateDoc ={$set:user}
+  //   const result=await usersCollection.updateOne(filter, updateDoc,options);
+  //   console.log('put',user);
+  //   res.send(result);
+  //   console.log(result);
+  // });
+  //admin
+  // app.put('/users/admin', async (req, res) => {
+  //   const user = req.body;
+  //   const filter = { email: user.email };
+  //   const updateDoc = { $set: { role: 'admin' } };
+  //   const result = await usersCollection.updateOne(filter, updateDoc);
+  //   console.log('put', user);
+  //   res.json(result);
+  //   // res.send(result);
+  //   console.log(result);
+  // });
+  // app.get('/users/:email', async (req, res) => {
+  //   const email = req.params.email;
+  //   const query = { email: email };
+  //   const user = await usersCollection.findOne(query);
+  //   let isAdmin = false;
+  //   if (user?.role === 'admin') {
+  //     isAdmin = true;
+  //   }
+  //   console.log(isAdmin);
+  //   res.json({ admin: isAdmin });
+  // });
   // adding new services
-
   app.post('/addServices', async (req, res) => {
     const service = req.body;
-    // console.log(service);
+    console.log(service);
     const result = await servicesCollection.insertOne(service);
     res.send(result);
   });
-
-  //all Services
+  // show all Services
   app.get('/allServices', async (req, res) => {
     const result = await servicesCollection.find({}).toArray();
     res.send(result);
   });
-
+  // //add review
+  // app.post('/review', async (req, res) => {
+  //   const product = req.body;
+  //   console.log(product);
+  //   const result = await reviewCollection.insertOne(product);
+  //   res.send(result);
+  // });
+  //show review
+  // app.get('/review', async (req, res) => {
+  //   const result = await reviewCollection.find({}).toArray();
+  //   res.send(result);
+  // });
   //each service load
-  app.get('/singleProduct/:ServiceId', async (req, res) => {
-    const result = await servicesCollection
-      .find({ _id: ObjectId(req.params.ServiceId) })
-      .toArray();
-    res.send(result[0]);
-  });
-
+  // app.get('/singleProduct/:ServiceId', async (req, res) => {
+  //   const result = await productsCollection
+  //     .find({ _id: ObjectId(req.params.ServiceId) })
+  //     .toArray();
+  //   res.send(result[0]);
+  // });
   //order
-  app.post('/confirmOrder', async (req, res) => {
-    const result = await bookingsCollection.insertOne(req.body);
-    res.send(result);
-  });
-
+  // app.post('/confirmOrder', async (req, res) => {
+  //   const result = await bookingsCollection.insertOne(req.body);
+  //   res.send(result);
+  // });
   //get myOrder
-
-  app.get('/myOrder/:email', async (req, res) => {
-    const result = await bookingsCollection
-      .find({ Email: req.params.email })
-      .toArray();
-    res.send(result);
-    console.log(result);
-  });
-
+  // app.get('/myOrder/:email', async (req, res) => {
+  //   const result = await bookingsCollection
+  //     .find({ Email: req.params.email })
+  //     .toArray();
+  //   res.send(result);
+  //   console.log(result);
+  // });
+  //delete product
+  // app.delete('/deleteProduct/:id', async (req, res) => {
+  //   console.log(req.params.id);
+  //   const result = await productsCollection.deleteOne({
+  //     _id: ObjectId(req.params.id),
+  //   });
+  //   console.log(result);
+  //   res.send(result);
+  // });
   //delete order
-
-  app.delete('/delteOrder/:id', async (req, res) => {
-    const result = await bookingsCollection.deleteOne({
-      _id: ObjectId(req.params.id),
-    });
-    res.send(result);
-  });
-
+  // app.delete('/deleteOrder/:id', async (req, res) => {
+  //   const result = await bookingsCollection.deleteOne({
+  //     _id: ObjectId(req.params.id),
+  //   });
+  //   res.send(result);
+  // });
   // all order
-  app.get('/allOrders', async (req, res) => {
-    const result = await bookingsCollection.find({}).toArray();
-    res.send(result);
-  });
-
+  // app.get('/allOrders', async (req, res) => {
+  //   const result = await bookingsCollection.find({}).toArray();
+  //   res.send(result);
+  // });
+  // app.post('/addBestProducts', async (req, res) => {
+  //   const product = req.body;
+  //   console.log(bestProductCollection);
+  //   const result = await bestProductCollection.insertOne(product);
+  //   res.send(result);
+  // });
+  // app.get('/getBestProducts', async (req, res) => {
+  //   const result = await bestProductCollection.find({}).toArray();
+  //   res.send(result);
+  // });
   // update statuses
-
-  app.put('/updateStatus/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedStatus = req.body.status;
-    const filter = { _id: ObjectId(id) };
-    console.log(updatedStatus);
-    bookingsCollection
-      .updateOne(filter, {
-        $set: { status: updatedStatus },
-      })
-      .then((result) => {
-        res.send(result);
-      });
-  });
+  // app.put('/updateStatus/:id', (req, res) => {
+  //   const id = req.params.id;
+  //   const updatedStatus = req.body.status;
+  //   const filter = { _id: ObjectId(id) };
+  //   console.log(updatedStatus);
+  //   bookingsCollection
+  //     .updateOne(filter, {
+  //       $set: { status: updatedStatus },
+  //     })
+  //     .then((result) => {
+  //       res.send(result);
+  //     });
+  // });
 });
 
 app.get('/', (req, res) => {
-  res.send('Running the Holiday Hype');
+  res.send('Running HolidayHype');
 });
 
 app.listen(port, () => {
